3ec4571f30a54e94227b52cd620a2ceae12c809f
MalakhovStas <ff9147782@gmail.com>
2023-03-29 19:34:01 +0300

    fix PostFeedback

--- buttons_and_messages/base_classes.py
+++ buttons_and_messages/base_classes.py
@@ -540,7 +540,9 @@ class PostFeedback(BaseButton):
                 result = await self.wb_api.send_feedback(
                     seller_token=seller_token,
                     x_supplier_id=feed_button.parent_name.lstrip('Supplier'),
-                    feedback_id=feed_button.class_name.lstrip('Feedback'),
+                    # При таком имени FeedbackkweEKYcB5HDhYKpHlFbv делает так weEKYcB5HDhYKpHlFbv т.е удаляет лишнийсимвол k
+                    # feedback_id=feed_button.class_name.lstrip('Feedback')
+                    feedback_id=feed_button.class_name.a.strip()[8:],  # Это работает хорошо
                     feedback_answer__text=feedback_answer_text,
                     update=update
                 )
